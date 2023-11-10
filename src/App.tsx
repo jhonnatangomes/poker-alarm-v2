d9110f5bbb7ae0dd1dd3ae3b2d17722d121b7f0a
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2023-11-10 11:01:08 -0300

    Improve add button and only starting tournaments that haven't passed enter time

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { ClockCard } from './components/ClockCard';
 import { assoc, mergeLeft, prop, propOr, reverse, sortBy } from 'ramda';
 import dayjs from 'dayjs';
 import { BsStopCircle, BsPlayCircle } from 'react-icons/bs';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
 
 type Clock = {
   remainingTime: number;
@@ -43,40 +44,56 @@ function App() {
         reverse(
           sortBy(
             propOr('finishTime', ''),
-            tournaments.map(tournament => ({
-              name: getClockTournamentName(tournament),
-              duration: 0,
-              remainingTime: 0,
-              isTicking: false,
-              tournament,
-              ...(isWithinStartingRange(tournament, now)
-                ? { finishTime: calculateEnterTime(tournament, now) }
-                : { disabled: true }),
-            })),
+            tournaments.map(tournament => {
+              const finishTime = calculateEnterTime(tournament, now);
+              const enterTimePassed = dayjs(finishTime).isBefore(now);
+              return {
+                name: getClockTournamentName(tournament),
+                duration: 0,
+                remainingTime: 0,
+                isTicking: false,
+                tournament,
+                ...(isWithinStartingRange(tournament, now) && !enterTimePassed
+                  ? { finishTime }
+                  : { disabled: true }),
+              };
+            }),
           ),
         ),
       ),
     );
   }, [tournaments]);
   return (
-    <div className='h-screen w-full flex flex-col justify-between'>
-      <div className='p-6 flex flex-wrap gap-4 overflow-y-auto'>
-        {clocks.map((clock, i) => (
-          <ClockCard key={i} {...clock} onPlay={onPlay(i)} onStop={onStop(i)} />
-        ))}
-        <NewClockCard onClick={openClockModal} />
-        {isDev && (
-          <>
-            <NewClockCard
-              onClick={createNewTestTournament}
-              label='Create Test Tournament'
+    <div className='h-screen w-full flex flex-col justify-between gap-6'>
+      <div className='p-6 flex justify-center overflow-y-auto relative'>
+        <div className='grid grid-cols-4 gap-4'>
+          {clocks.map((clock, i) => (
+            <ClockCard
+              key={i}
+              {...clock}
+              onPlay={onPlay(i)}
+              onStop={onStop(i)}
             />
-            <NewClockCard
-              onClick={batchCreateTestTournaments}
-              label='Batch Create Test Tournaments'
-            />
-          </>
-        )}
+          ))}
+          {isDev && (
+            <>
+              <NewClockCard
+                onClick={createNewTestTournament}
+                label='Create Test Tournament'
+              />
+              <NewClockCard
+                onClick={batchCreateTestTournaments}
+                label='Batch Create Test Tournaments'
+              />
+            </>
+          )}
+        </div>
+        <div className='fixed bottom-20 right-6 flex justify-center items-center h-20 w-20'>
+          <BsFillPlusCircleFill
+            onClick={openClockModal}
+            className='text-green-600 w-16 h-16 cursor-pointer'
+          ></BsFillPlusCircleFill>
+        </div>
       </div>
       <div className='bg-grayCard p-2 flex items-center justify-center'>
         {anyClocksTicking ? (
